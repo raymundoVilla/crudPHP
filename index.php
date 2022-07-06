9c467223ece6f612287cadf73dbb884982b1fa16
Raymundo Villanueva <blod.plos.01@gmail.com>
2022-07-05 19:01:45 -0500

    se segmento la pagina del index para un mejor manejo de las vistas

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<?php include('conexion.php')?>
-<!DOCTYPE html>
-<html lang="en">
+<?php include('./template/header.php')?>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-
-        <title>Crud</title>
-    </head>
-
-    <body>
         <h1>Crud Realizado con PHP crudo</h1>
+
         <!-- formulario -->
        <div class="container d-flex">
         <div class="container">
@@ -71,6 +59,4 @@
         <!-- end table de datos -->
         </div>
 
-    </body>
-
-</html>
+        <?php include('./template/footer.php');?>
